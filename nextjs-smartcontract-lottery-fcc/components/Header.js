3560739b4c5aa85fc840e74e11f698d4d4823818
John Lawniczak <105305546+the-vegetarian-vampire@users.noreply.github.com>
2023-05-16 20:08:03 -0400

    Add files via upload

---
+++ nextjs-smartcontract-lottery-fcc/components/Header.js
@@ -0,0 +1,13 @@
+import { ConnectButton } from "web3uikit"
+
+// yarn add web3uikit // library gives pop-up modal
+export default function Header() {
+    return (
+        <nav className="p-5 border-b-2 flex flex-row">
+            <h1 className="py-4 px-4 font-bold text-3xl"> Decentralized Lottery</h1>
+            <div className="ml-auto py-2 px-4">
+                <ConnectButton moralisAuth={false} />
+            </div>
+        </nav>
+    )
+}
